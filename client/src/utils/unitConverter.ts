e8aafa0c46e5fe441c0bcc5de0ca8bf1349fb7a8
Joshua Chagani <joshua@joshuachagani.com>
2021-08-14 15:35:32 -0500

    ✏️ Fix incorrect conversion

--- client/src/utils/unitConverter.ts
+++ client/src/utils/unitConverter.ts
@@ -1,15 +1,14 @@
 const KILOGRAM_TO_POUND = 2.20462
 const METER_TO_FEET = 3.28084
-const SHIFT_TO_KILOGRAM = 10
 
 export const gramsToPounds = (weight: number): string => {
-	const conversion = (weight / SHIFT_TO_KILOGRAM) * KILOGRAM_TO_POUND
+	const conversion = weight * KILOGRAM_TO_POUND
 	const toFormat = new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 })
 
 	return toFormat.format(conversion)
 }
 
 export const metricToFeet = (meters: number): number => {
-	const conversion = (meters / 10) * METER_TO_FEET
+	const conversion = meters * METER_TO_FEET
 	return parseFloat(conversion.toFixed(2))
 }
